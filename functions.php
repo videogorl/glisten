1a10a68bc79fdfd759d9e5aa0180b06b113c4cc1
Andreas Reif <andreas@sitecare.com>
2023-12-28 11:07:51 -0700

    Enqueue block styles

---
+++ functions.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Glisten functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Glisten
+ * @since Glisten 1.0
+ */
+namespace Glisten;
+
+/**
+ * Load custom block styles only when the block is used.
+ */
+function enqueue_custom_block_styles() {
+
+	// Scan our styles folder to locate block styles.
+	$files = glob( get_template_directory() . '/assets/styles/*.css' );
+
+	foreach ( $files as $file ) {
+
+		// Get the filename and core block name.
+		$filename   = basename( $file, '.css' );
+		$block_name = str_replace( 'core-', 'core/', $filename );
+
+		wp_enqueue_block_style(
+			$block_name,
+			array(
+				'handle' => "ollie-block-{$filename}",
+				'src'    => get_theme_file_uri( "assets/styles/{$filename}.css" ),
+				'path'   => get_theme_file_path( "assets/styles/{$filename}.css" ),
+			)
+		);
+	}
+}
+add_action( 'init', __NAMESPACE__ . '\enqueue_custom_block_styles' );
