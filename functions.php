fb345926fc648d5965e7afe8ba05aa9057bdfdb4
Andreas Reif <andreas@sitecare.com>
2023-12-28 15:02:28 -0700

    Enqueue theme styles Add block style variations

--- functions.php
+++ functions.php
@@ -9,6 +9,26 @@
  */
 namespace Glisten;
 
+
+/**
+ * Set up theme defaults and register various WordPress features.
+ */
+function setup() {
+
+	// Enqueue editor styles and fonts.
+	add_editor_style( 'style.css' );
+}
+add_action( 'after_setup_theme', __NAMESPACE__ . '\setup' );
+
+
+/**
+ * Enqueue styles.
+ */
+function enqueue_style_sheet() {
+	wp_enqueue_style( sanitize_title( __NAMESPACE__ ), get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+}
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_style_sheet' );
+
 /**
  * Load custom block styles only when the block is used.
  */
@@ -34,3 +54,29 @@ function enqueue_custom_block_styles() {
 	}
 }
 add_action( 'init', __NAMESPACE__ . '\enqueue_custom_block_styles' );
+
+
+/**
+ * Add block style variations.
+ */
+function register_block_styles() {
+
+	$block_styles = array(
+		'core/post-terms'                    => array(
+			'icon-only' 	=> __( 'Icon Only', 'glisten' ),
+		)
+	);
+
+	foreach ( $block_styles as $block => $styles ) {
+		foreach ( $styles as $style_name => $style_label ) {
+			register_block_style(
+				$block,
+				array(
+					'name'  => $style_name,
+					'label' => $style_label,
+				)
+			);
+		}
+	}
+}
+add_action( 'init', __NAMESPACE__ . '\register_block_styles' );
