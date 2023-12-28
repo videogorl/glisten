5356bf86912babbceaf63851ec251a45b0d530ea
Andreas Reif <andreas@sitecare.com>
2023-12-28 11:11:04 -0700

    Update nav and footer designs Add patterns

---
+++ patterns/footer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: glisten/footer
+ * Categories: footer
+ * Viewport width: 750
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|180","bottom":"var:preset|spacing|180"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--180);margin-bottom:var(--wp--preset--spacing--180)"><!-- wp:site-title {"level":2} /-->
+
+<!-- wp:navigation {"overlayMenu":"never"} /-->
+
+<!-- wp:social-links {"iconColor":"contrast","iconColorValue":"rgb(255, 255, 255)","openInNewTab":true,"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"left"}} -->
+<ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"facebook.com/andreasjr","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"twitter.com/andreas_jr","service":"twitter"} /-->
+
+<!-- wp:social-link {"url":"instagram.com/andreasjr","service":"instagram"} /-->
+
+<!-- wp:social-link {"url":"https://linkedin.com/in/andreasjr","service":"linkedin"} /-->
+
+<!-- wp:social-link {"url":"youtube.com/charliemcreif","service":"youtube","label":""} /-->
+
+<!-- wp:social-link {"url":"https://mastodon.social/@andreasjr","service":"mastodon","rel":"me"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
---
+++ patterns/header.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Header
+ * Slug: glisten/header
+ * Categories: header
+ * Viewport width: 750
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"1.25rem","bottom":"1.25rem","left":"var(--wp--style--root--padding-left)","right":"var(--wp--style--root--padding-right)"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="padding-top:1.25rem;padding-right:var(--wp--style--root--padding-right);padding-bottom:1.25rem;padding-left:var(--wp--style--root--padding-left)"><!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull"><!-- wp:site-logo {"width":50,"shouldSyncIcon":true} /-->
+
+<!-- wp:navigation {"overlayMenu":"always","icon":"menu","overlayBackgroundColor":"base","overlayTextColor":"contrast","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"center","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"0"}}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
