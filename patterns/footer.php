787aef62662a3ec078e6770dc66cf8c65de20f89
Andreas Reif <andreas@sitecare.com>
2023-12-28 18:25:37 -0700

    Updated content

--- patterns/footer.php
+++ patterns/footer.php
@@ -6,10 +6,10 @@
  * Viewport width: 750
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|180","bottom":"var:preset|spacing|180"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--180);margin-bottom:var(--wp--preset--spacing--180)"><!-- wp:site-title {"level":2} /-->
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|90","bottom":"var:preset|spacing|90"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--90);margin-bottom:var(--wp--preset--spacing--90)"><!-- wp:site-title {"level":2} /-->
 
-<!-- wp:navigation {"overlayMenu":"never"} /-->
+<!-- wp:navigation {"ref":276,"overlayMenu":"never","className":"is-style-text-action"} /-->
 
 <!-- wp:social-links {"iconColor":"contrast","iconColorValue":"rgb(255, 255, 255)","openInNewTab":true,"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"left"}} -->
 <ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"facebook.com/andreasjr","service":"facebook"} /-->
