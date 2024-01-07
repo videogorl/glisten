26274c628382db2f6dca1174ac3ed0e0b3601f39
Andreas Reif <andreas@nysics.com>
2024-01-07 14:57:17 -0700

    Add gorups to wrap logo and nav so that if there is no site logo it'll show

--- patterns/header.php
+++ patterns/header.php
@@ -6,10 +6,14 @@
  * Viewport width: 750
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"1.25rem","bottom":"1.25rem","left":"var(--wp--style--root--padding-left)","right":"var(--wp--style--root--padding-right)"}}},"layout":{"type":"default"}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"1.25rem","bottom":"1.25rem","left":"var(\u002d\u002dwp\u002d\u002dstyle\u002d\u002droot\u002d\u002dpadding-left)","right":"var(\u002d\u002dwp\u002d\u002dstyle\u002d\u002droot\u002d\u002dpadding-right)"}}},"layout":{"type":"default"}} -->
 <div class="wp-block-group" style="padding-top:1.25rem;padding-right:var(--wp--style--root--padding-right);padding-bottom:1.25rem;padding-left:var(--wp--style--root--padding-left)"><!-- wp:group {"align":"full","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignfull"><!-- wp:site-logo {"width":50,"shouldSyncIcon":true} /-->
+<div class="wp-block-group alignfull"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:site-logo {"width":50,"shouldSyncIcon":true} /--></div>
+<!-- /wp:group -->
 
-<!-- wp:navigation {"overlayMenu":"always","icon":"menu","overlayBackgroundColor":"base","overlayTextColor":"contrast","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"center","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"0"}}}} /--></div>
+<!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:navigation {"ref":4,"overlayMenu":"always","icon":"menu","overlayBackgroundColor":"base","overlayTextColor":"contrast","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"center","orientation":"horizontal"},"style":{"spacing":{"margin":{"top":"0"}}}} /--></div>
+<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
