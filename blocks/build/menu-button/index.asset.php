4deb431a85e5c2a29f5f453d80a2fb13a878bbb3
Andreas Reif <andreas@sitecare.com>
2024-04-13 13:22:54 -0600

    Latest build

--- blocks/build/menu-button/index.asset.php
+++ blocks/build/menu-button/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'a065fabd5a8163c93c03');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '3008b949ce303f33107d');
